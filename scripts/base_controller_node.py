8920453a28362d715b4390f16415a04dc275033e
Aldo Vargas <aldo.vargas@railvision.ca>
2025-04-19 21:05:57 -0400

    Removing the time offset

--- scripts/base_controller_node.py
+++ scripts/base_controller_node.py
@@ -162,7 +162,7 @@ class BaseController:
         self.odom_broadcaster.sendTransform(
             (self.x, self.y, 0),
             q,
-            rospy.Time.now() + rospy.Duration(0.0001),  # 100μs offset to avoid duplicate,
+            current_time
             "base_link",
             "odom"
         )
